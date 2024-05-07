88befb947b90b7f958b89a16d6269e399800a245
alexkokobane <63163413+alexkokobane@users.noreply.github.com>
2024-05-07 08:57:53 +0200

    fixes films display bugs

--- src/lib/FilmsCard.svelte
+++ src/lib/FilmsCard.svelte
@@ -12,9 +12,9 @@
 
 <section class="section">
 	{#if viewMorePath.length > 0}
-		<div class="title-band heading">
-			<h2 class="uni-pad">{heading}</h2>
-			<a href="{viewMorePath}">View more</a>
+		<div class="title-band heading uni-pad">
+			<h2>{heading}</h2>
+			<a class="button-regular" href="{viewMorePath}">View More</a>
 		</div>
 	{:else}
 		<h2 class="title-band heading">{heading}</h2>
@@ -39,7 +39,7 @@
 		{:else}
 			<EmptyState 
 				text="Nothing to see here, yet." 
-				height="70vh"
+				height="40vh"
 				fill="var(--base-color-alt)"
 			/>
 		{/if}
@@ -137,7 +137,7 @@
 	}
 
 	.section {
-		margin: 1rem 0;
+		margin: 2rem 0;
 		min-height: 10rem;
 	}
 
